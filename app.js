a48eab08248aea940e6d3c50b6ec7bd0b5ef7b54
nick-lloyd-test <137975460+nick-lloyd-test@users.noreply.github.com>
2023-06-28 16:14:21 +1000

    Update app.js

--- app.js
+++ app.js
@@ -4,7 +4,7 @@ const port = process.env.PORT || 3001;
 
 app.get("/", (req, res) => res.type('html').send(html));
 
-app.get("/api", (req, res) => res.json({yes: 'Sir'}));
+app.get("/api", (req, res) => res.type('json').json({yes: 'Sir'}));
 
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
