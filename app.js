24d21afe594b30e3c1965de8ed720078dbbf1b2b
nick-lloyd-test <137975460+nick-lloyd-test@users.noreply.github.com>
2023-06-28 18:01:15 +1000

    Update app.js

--- app.js
+++ app.js
@@ -6,6 +6,8 @@ app.get("/", (req, res) => res.type('html').send(html));
 
 app.get("/api", (req, res) => res.status(200).type('json').json({yes: 'Sir again'}));
 
+console.log('Port: ' + port);
+
 const server = app.listen(port, () => console.log(`Example app listening on port ${port}!`));
 
 server.keepAliveTimeout = 120 * 1000;
